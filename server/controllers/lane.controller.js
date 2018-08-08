ea7a38e59079f84a55c62e32a1ef425d59328069
Karolina Madej <kkarolinamadej@gmail.com>
2018-08-08 14:46:07 +0200

    Add addLane function in lane controller

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -1,5 +1,23 @@
 import Lane from '../models/lane';
+import uuid from 'uuid';
 
 export function getSomething(req, res) {
   return res.status(200).end();
+}
+
+export function addLane(req, res) {
+  if (!req.body.name) {
+    res.status(403).end();
+  }
+
+  // jeżeli "ciało zapytania" nie posiada name to error 403, jeśli posiada, to tworzymy newLane na podstawie "ciała zapytania"
+  const newLane = new Lane(req.body);
+  newLane.notes = [];
+  newLane.id = uuid();
+  newLane.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json(saved);
+  });
 }
