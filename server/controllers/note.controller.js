80763e243a6c76fd4ff5cbb928e211439301158c
Karolina Madej <kkarolinamadej@gmail.com>
2018-08-09 12:32:50 +0200

    Add adding note

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -1,12 +1,14 @@
 import Note from '../models/note';
+import Lane from '../models/lane';
 import uuid from 'uuid';
 
 export function getSomething(req, res) {
   return res.status(200).end();
 }
 
+
 export function addNote(req, res) {
-  const ( note, laneId ) = req.body;
+  const { note, laneId } = req.body;
 
   if (!note || !note.task || !laneId) {
     res.status(400).end();
@@ -16,5 +18,18 @@ export function addNote(req, res) {
     task: note.task,
   });
 
-  newNote.id = uuid()
+  newNote.id = uuid();
+  newNote.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    Lane.findOne({ id: laneId })
+      .then(lane => {
+        lane.notes.push(saved);
+        return lane.save();
+      })
+      .then(() => {
+        res.json(saved);
+      });
+  });
 }
